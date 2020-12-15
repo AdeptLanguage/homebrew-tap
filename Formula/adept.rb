8aa812aea6e9de06f645df6aa9852fd555281412
Seeker <meaningseeking@protonmail.com>
2020-12-14 18:22:49 -0800

    Add formula (head-only)

---
+++ Formula/adept.rb
@@ -0,0 +1,34 @@
+class Adept < Formula
+  desc "Blazing fast language for general purpose programming"
+  homepage "https://github.com/IsaacShelton/Adept"
+  license "GPL-3.0"
+
+  head do
+    url "https://github.com/IsaacShelton/Adept.git"
+
+    resource "import" do
+      url "https://github.com/IsaacShelton/AdeptImport.git"
+    end
+  end
+
+  depends_on "cmake" => :build
+  depends_on "llvm@9"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    bin.install "adept"
+    (bin/"import").install resource("import")
+  end
+
+  test do
+    (testpath/"test.adept").write <<~EOS
+      import '2.3/basics.adept'
+      func main {
+        print("Hello World!")
+      }
+    EOS
+    system bin/"adept", "test.adept"
+    assert_match "Hello World!", shell_output("./test")
+  end
+end
