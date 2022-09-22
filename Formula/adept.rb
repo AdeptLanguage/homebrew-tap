1cd4fb3d8a06507fc7eca2ae91558eab5872a35c
IsaacShelton <isaac.a.shelton@gmail.com>
2022-09-21 22:41:46 -0500

    Add v2.7 as `stable`

--- Formula/adept.rb
+++ Formula/adept.rb
@@ -8,13 +8,13 @@ class Adept < Formula
   license "GPL-3.0-only"
 
   stable do
-    url "https://github.com/AdeptLanguage/Adept/archive/v2.6.tar.gz"
-    sha256 "e1ff2e9efe20b07a2dc269742fea672b277675d127e578c8e5c107b4c12886ca"
+    url "https://github.com/AdeptLanguage/Adept/archive/v2.7.tar.gz"
+    sha256 "74b2bef3e9adddc88ecabc9237d178236fc2e21d342e527cea2e031450bb0899"
     depends_on "llvm@13"
 
     resource "import" do
-      url "https://github.com/AdeptLanguage/AdeptImport/archive/v2.6.tar.gz"
-      sha256 "a7a6e16d55ea78e4962bf77fa876df73275f3baf8b9636198e48bcec7bc2edc2"
+      url "https://github.com/AdeptLanguage/AdeptImport/archive/v2.7.tar.gz"
+      sha256 "87a02463d283c324c47f552e0569b5cbc74027e7dde557989acb9b334e2115c0"
     end
 
     # Lazily grab latest configuration
@@ -24,7 +24,7 @@ class Adept < Formula
   end
 
   bottle do
-    root_url "https://github.com/AdeptLanguage/homebrew-tap/releases/download/adept-2.6"
+    root_url "https://github.com/AdeptLanguage/homebrew-tap/releases/download/adept-2.7"
     sha256 cellar: :any,                 big_sur:      "4dbfcceb6c7cef0b3807716f6714ccbd90fa30c76385cec8c60355e18645dd61"
     sha256 cellar: :any,                 catalina:     "8b7722dc40d4d4326798591b38d76c45a202a2e27c3eb021462f56d9990aa297"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "c4f63829296592b218f9862ce8c00eea34308bf241adb99064c9b2552c7c251a"
